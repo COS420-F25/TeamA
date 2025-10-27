9dd4c212fa717de96d2b9a9e94b9a47947ee77b4
Joshua Deveau <edward.deveau@maine.edu>
2025-10-26 20:15:31 -0400

    Removed the React spinning logo. Finished the config file based on the video

--- careerwise/src/firebase-config.tsx
+++ careerwise/src/firebase-config.tsx
@@ -2,6 +2,10 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
+import {getAuth} from "firebase/auth"
+import {getFirestore} from "firebase/firestore"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,3 +21,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app); //Get authentication token
+
+export const db = getFirestore(app); //Export as a database so other files can use it
