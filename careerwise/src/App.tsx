7efb3e837ebd7606b5c449c75c27abb4dc6063c4
Joshua Deveau <edward.deveau@maine.edu>
2025-10-26 20:35:55 -0400

    Added a value signInWithGoogle and made it clickable. Because ofthe imports sign in with google actually works

--- careerwise/src/App.tsx
+++ careerwise/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import {auth} from "./firebase-config";
+import {useSignInWithGoogle} from "react-firebase-hooks/auth"
 
 function App() {
+
+  //We need 4 values
+  //signInWithGoogle() modifies the state. User state will be modified later
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
   return (
     <div className="App">
-    Log in
+      {/*Add sign in button */}
+      <button onClick={() => signInWithGoogle()}>Sign In</button>
     </div>
   );
 }
