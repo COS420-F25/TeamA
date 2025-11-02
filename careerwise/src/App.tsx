aca343dfe361e1d65eea014669779c85d50ac7af
Brady Dube <brady.dube@maine.edu>
2025-11-02 17:29:15 -0500

    Added Login component with email and password fields and Google button

--- careerwise/src/App.tsx
+++ careerwise/src/App.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import {auth} from "./firebase-config";
 import {useSignInWithGoogle} from "react-firebase-hooks/auth"
+import { LoginField } from "./components/Login"
+import { GoogleLoginButton } from "./components/Login"
 
 function App() {
 
+
+  /* Use state for email and password from user */
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  // Sign in with Google
   //We need 4 values
   //signInWithGoogle() modifies the state. User state will be modified later
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
   return (
     <div className="App">
-      {/*Add sign in button */}
-      <button onClick={() => signInWithGoogle()}>Sign In</button>
+      {/* Log in fields */}
+	  <LoginField setEmail={setEmail} setPassword={setPassword}></LoginField>
+	  {/* Google Sign in Button */}
+	  <GoogleLoginButton onclick={() => signInWithGoogle()}></GoogleLoginButton>
     </div>
   );
 }
